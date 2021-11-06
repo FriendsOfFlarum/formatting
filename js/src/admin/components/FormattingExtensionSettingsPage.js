f9b2c9f84381e47b6e906c759bc51fcee206bb9d
David Wheatley <hi@davwheat.dev>
2021-11-06 15:41:53 +0000

    chore: repo maintenance

--- js/src/admin/components/FormattingExtensionSettingsPage.js
+++ js/src/admin/components/FormattingExtensionSettingsPage.js
@@ -3,35 +3,31 @@ import ExtensionPage from 'flarum/admin/components/ExtensionPage';
 import LinkButton from 'flarum/common/components/LinkButton';
 
 export default class FormattingExtensionSettingsPage extends ExtensionPage {
-    oninit(vnode) {
-        super.oninit(vnode);
-    }
+  oninit(vnode) {
+    super.oninit(vnode);
+  }
 
-    content() {
-        return [
-            <div className="container">
-                <div className="FoFFormattingSettingsPage">
-                    <div className="Form">
-                        {app.forum.attribute('fof-formatting.plugins').map((plugin) =>
-                            this.buildSettingComponent({
-                                type: 'boolean',
-                                setting: `fof-formatting.plugin.${plugin.toLowerCase()}`,
-                                label: (
-                                    <LinkButton
-                                        href={`https://s9etextformatter.readthedocs.io/Plugins/${plugin}/Synopsis`}
-                                        external={true}
-                                        target="_blank"
-                                    >
-                                        {plugin}
-                                    </LinkButton>
-                                ),
-                                help: app.translator.trans(`fof-formatting.admin.plugins.${plugin}`),
-                            })
-                        )}
-                        <div className="Form-group">{this.submitButton()}</div>
-                    </div>
-                </div>
-            </div>,
-        ];
-    }
+  content() {
+    return [
+      <div className="container">
+        <div className="FoFFormattingSettingsPage">
+          <div className="Form">
+            {app.forum.attribute('fof-formatting.plugins').map((plugin) =>
+              this.buildSettingComponent({
+                type: 'boolean',
+                setting: `fof-formatting.plugin.${plugin.toLowerCase()}`,
+                label: (
+                  <LinkButton href={`https://s9etextformatter.readthedocs.io/Plugins/${plugin}/Synopsis`} external={true} target="_blank">
+                    {plugin}
+                  </LinkButton>
+                ),
+                help: app.translator.trans(`fof-formatting.admin.plugins.${plugin}`),
+              })
+            )}
+            <div className="Form-group">{this.submitButton()}</div>
+          </div>
+        </div>
+      </div>,
+    ];
+  }
 }
--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,5 +2,5 @@ import app from 'flarum/admin/app';
 import FormattingExtensionSettingsPage from './components/FormattingExtensionSettingsPage';
 
 app.initializers.add('fof/formatting', () => {
-    app.extensionData.for('fof-formatting').registerPage(FormattingExtensionSettingsPage);
+  app.extensionData.for('fof-formatting').registerPage(FormattingExtensionSettingsPage);
 });
