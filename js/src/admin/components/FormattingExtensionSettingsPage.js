ed862f7b18059fe9609ebfcd74d6ccd349471c50
IanM <ian@morland.me>
2025-02-16 13:08:32 +0000

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/components/FormattingExtensionSettingsPage.js
+++ js/src/admin/components/FormattingExtensionSettingsPage.js
@@ -1,3 +1,4 @@
+import Form from 'flarum/common/components/Form';
 import app from 'flarum/admin/app';
 import ExtensionPage from 'flarum/admin/components/ExtensionPage';
 import LinkButton from 'flarum/common/components/LinkButton';
@@ -11,21 +12,23 @@ export default class FormattingExtensionSettingsPage extends ExtensionPage {
     return [
       <div className="container">
         <div className="FoFFormattingSettingsPage">
-          <div className="Form">
+          <Form>
             {app.forum.attribute('fof-formatting.plugins').map((plugin) =>
               this.buildSettingComponent({
                 type: 'boolean',
                 setting: `fof-formatting.plugin.${plugin.toLowerCase()}`,
+
                 label: (
                   <LinkButton href={`https://s9etextformatter.readthedocs.io/Plugins/${plugin}/Synopsis`} external={true} target="_blank">
                     {plugin}
                   </LinkButton>
                 ),
+
                 help: app.translator.trans(`fof-formatting.admin.plugins.${plugin}`),
               })
             )}
             <div className="Form-group">{this.submitButton()}</div>
-          </div>
+          </Form>
         </div>
       </div>,
     ];
